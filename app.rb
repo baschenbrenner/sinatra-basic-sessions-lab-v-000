add25415302b0b5be8b7cd65faf3cd490a3903f2
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-11-13 20:36:47 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -10,13 +10,13 @@ class App < Sinatra::Base
 
 
   get '/' do
-    @session = session
+    
     erb :index
   end
 
 
   post '/checkout' do
-    
+    @session = session
     erb :checkout
   end
 end
